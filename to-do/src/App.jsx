5970c06d9f2b7b7bcbe6790c4bc931cc0246b971
SullyDurgin <katsdurgin@gmail.com>
2022-03-07 20:58:29 -0500

    todos show up in console

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import TodoList from './TodoList'
 
 function App() {
   const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
 
   function handleAddTodo(e) {
-    
+    const name = todoNameRef.current.value
+    if (name == '') return
+    console.log(name)
+    todoNameRef.current.value = null
   }
 
 	return (
     <>
      <TodoList todos={todos} />
-     <input type="text" />
+     <input ref={todoNameRef} type="text" />
      <button onClick={handleAddTodo}>Add Task</button>
      <button>Clear Completed Tasks</button>
      <div>0 Tasks Left To Do</div>
